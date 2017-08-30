6c1fd210b48fb9362c1ab4a8ecce2fcae752fc76
Jake Ryan Smith <jakeryansmith@gmail.com>
2017-08-29 20:50:00 -0400

    Various updates.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,15 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.less('src/less/pasture.less', 'dist/');
+// Mix Graze CSS framework
+mix.less('src/graze-css/graze-css.less', 'dist/');
+mix.copy('dist/graze-css.css', '/Users/jakeryansmith/Code/grazecart/public/css/graze.css');
+mix.copy('dist/graze-css.css', '/Users/jakeryansmith/Code/gcmarketing/public/css/graze.css');
 
-mix.copy('dist/pasture.css', '/Users/jakeryansmith/Code/grazecart/public/css');
+// Mix Theme Editor
+mix.less('src/theme-editor/theme-editor.less', 'dist/');
+mix.copy('dist/theme-editor.css', '/Users/jakeryansmith/Code/grazecart/public/css/');
+
+// Mix Template Editor
+mix.less('src/template-editor/template-editor.less', 'dist/');
+mix.copy('dist/template-editor.css', '/Users/jakeryansmith/Code/grazecart/public/css/');
