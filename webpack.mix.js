35540a8d960d2b002e107b3d6a40a7da2aa6d603
Jake Ryan Smith <jakeryansmith@gmail.com>
2017-08-31 16:46:46 -0400

    Add login page. Other small improvements.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,5 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application, as well as bundling up your JS files.
- |
- */
 
 // Mix Graze CSS framework
 mix.less('src/graze-css/graze-css.less', 'dist/');
